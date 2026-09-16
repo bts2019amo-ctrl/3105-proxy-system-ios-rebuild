6c64a0ea974b3115b057eb827dba64f36897792a
bts2019amo-ctrl <3.21654992e+08+bts2019amo-ctrl@users.noreply.github.com>
2026-09-16 17:14:05 +0000

    Revert "Protect patch workspaces with iOS data protection"
    This reverts commit 580746aa36c2e0e9271997ef3a06426d9e8a88d5.

--- ThreeOneOSFive/helpers/PatchWorkspaceService.swift
+++ ThreeOneOSFive/helpers/PatchWorkspaceService.swift
@@ -23,7 +23,6 @@ enum PatchWorkspaceService {
         let documents = try documentsRootURL(fileManager: fileManager)
         let root = documents.appendingPathComponent("Patches", isDirectory: true)
         try fileManager.createDirectory(at: root, withIntermediateDirectories: true)
-        try protect(url: root, fileManager: fileManager)
         return root
     }
 
@@ -98,7 +97,6 @@ enum PatchWorkspaceService {
                 workspaceURL: staging
             )
             try fileManager.moveItem(at: staging, to: destination)
-            try protect(url: destination, fileManager: fileManager)
             return destination
         } catch let error as PatchPackageError {
             throw error
@@ -329,22 +327,6 @@ enum PatchWorkspaceService {
             return
         }
         try fileManager.removeItem(at: workspace)
-    }
-
-    private static func protect(url: URL, fileManager: FileManager) throws {
-        try fileManager.setAttributes(
-            [.protectionKey: FileProtectionType.completeUnlessOpen],
-            ofItemAtPath: url.path
-        )
-        guard let enumerator = fileManager.enumerator(at: url, includingPropertiesForKeys: nil) else {
-            return
-        }
-        for case let child as URL in enumerator {
-            try fileManager.setAttributes(
-                [.protectionKey: FileProtectionType.completeUnlessOpen],
-                ofItemAtPath: child.path
-            )
-        }
     }
 
     private static func workspaceTarget(
